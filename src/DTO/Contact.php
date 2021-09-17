75b280b5f5c8143880d04d7a0c7c9ddab70e707f
Pavel Babushkin <darkdarin@tochka.com>
2021-09-17 15:27:18 +0500

    Fix required fields on Contact DTO

--- src/DTO/Contact.php
+++ src/DTO/Contact.php
@@ -12,12 +12,12 @@ final class Contact extends DataTransferObject
     /**
      * The identifying name of the contact person/organization.
      */
-    public string $name;
+    public ?string $name;
     
     /**
      * The URL pointing to the contact information. MUST be in the format of a URL.
      */
-    public string $url;
+    public ?string $url;
     
     /**
      * The email address of the contact person/organization. MUST be in the format of an email address.
