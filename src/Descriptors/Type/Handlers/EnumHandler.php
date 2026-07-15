8070119b581247107250df737c9d3c8d356fde69
hatsu <hatsu@tochka.com>
2026-07-15 14:35:27 +0500

    feat: расшифровки для элементов enum

--- src/Descriptors/Type/Handlers/EnumHandler.php
+++ src/Descriptors/Type/Handlers/EnumHandler.php
@@ -2,6 +2,8 @@
 
 namespace Tochka\OpenRpc\Descriptors\Type\Handlers;
 
+use phpDocumentor\Reflection\DocBlock\Tags\Var_;
+use phpDocumentor\Reflection\DocBlockFactory;
 use Tochka\OpenRpc\Descriptors\Type\SingularTypeInfo;
 use Tochka\OpenRpc\Descriptors\Type\TypeDescriptor;
 use Tochka\OpenRpc\DTO\Schema;
@@ -21,9 +23,32 @@ class EnumHandler implements HandlerInterface
         /** @var \BackedEnum $className */
         $className = $info->className;
         $schema->enum = $className::cases();
+        $ref = new \ReflectionEnum($className);
+        $cases = $ref->getCases();
+        $docFactory = DocBlockFactory::createInstance();
+        foreach ($cases as $case) {
+            $docText = $case->getDocComment();
+            $descriptionText = '';
+            if ($docText) {
+                $docBlock = $docFactory->create($docText);
+                /** @var Var_ $tag */
+                $tag = $docBlock->getTagsByName('var')[0] ?? null;
+                if ($tag) {
+                    $descriptionText = $tag->getDescription()?->render() ?? '';
+                }
+            }
+            // если не удалось достать из тега, возможно там просто текст
+            if (!$descriptionText && $docText) {
+                $descriptionText = $docBlock->getSummary();
+            }
+            $schema->oneOf[] = [
+                'title' => $case->getName(),
+                'const' => $case->getBackingValue(),
+                'description' => $descriptionText,
+            ];
+        }
         
-        $reflector = new \ReflectionEnum($className);
-        $valueType = $reflector->getBackingType();
+        $valueType = $ref->getBackingType();
         if ($valueType === null) {
             $schema->type = ['string', 'integer'];
         } else {
